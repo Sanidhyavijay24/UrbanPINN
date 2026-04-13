73c6544ed8d12ecfc934753d39b5044e4a5a3529
Sanidhya Vijay <sanidhyavijay2004@gmail.com>
2026-04-13 17:01:47 +0530

    Fix: Remove invalid ThreeJS imports blocking Vercel Typescript build

--- frontend/src/components/canvas/Scene.tsx
+++ frontend/src/components/canvas/Scene.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, PerspectiveCamera, Grid, EffectComposer, Bloom } from '@react-three/drei';
+import { OrbitControls, PerspectiveCamera, Grid } from '@react-three/drei';
 import { Buildings } from './Buildings';
 import { WindField } from './WindField';
 import { ThermalMap } from './ThermalMap';
